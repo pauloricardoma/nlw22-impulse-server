424db3ada40118a1495a96cf65714c8b99a41b37
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-05-16 13:14:44 -0300

    deploy nlw22 server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bd0259a3d52846",
+    pass: "b49f7f340dfa43"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Paul Rick <aki7ae@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import { routes } from './routes';
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'exp://192.168.0.130:19000', 'http://localhost:19002']
+}));
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('HTTP server running!');
+});
+
+// SQLite
+// Prisma
