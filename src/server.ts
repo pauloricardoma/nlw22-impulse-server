cc260f57a29a5ebe7a4ef35b87ee92e656aa40c5
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-05-16 14:21:29 -0300

    Add cors Vercel

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,7 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://nlw22-impulse-web.vercel.app/']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
