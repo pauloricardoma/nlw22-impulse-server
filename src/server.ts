c2f7e88830449698223db2daad534e5a42c04b75
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-05-16 14:50:38 -0300

    Add cors Vercel

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import { routes } from './routes';
 
 const app = express();
 
-const whiteList = ['https://nlw22-impulse-web.vercel.app/'];
+const whiteList = [
+  'https://nlw22-impulse-web.vercel.app/', 
+  'https://nlw22-impulse-lxv6frf3v-pauloricardoma.vercel.app',
+];
 const corsOptions = {
   origin(origin: any, callback: any) {
     if (whiteList.indexOf(origin) !== -1 || !origin) {
@@ -18,7 +21,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(routes);
+app.use(routes, cors(corsOptions));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server running!');
